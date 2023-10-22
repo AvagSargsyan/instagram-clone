37266e2409d8c409d858ee2aeb77b006c8a2eb8a
avag <avagsargsyan01@gmail.com>
2023-10-22 20:02:34 +0400

    Add custom error handler

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,29 +1,35 @@
+import asyncHandler from 'express-async-handler';
+
 // req: GET /api/posts
-const getPosts = (req, res) => {
+const getPosts = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: 'Get posts'
   });
-};
+});
 
 // req: POST /api/posts
-const addPost = (req, res) => {
+const addPost = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Please add a text field');
+  }
   res.status(200).json({
     message: 'Set post'
   });
-};
+});
 
 // req: PUT /api/posts/:id
-const updatePost = (req, res) => {
+const updatePost = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Update post ${req.params.id}`
   });
-};
+});
 
 // req: DELETE /api/posts/:id
-const deletePost = (req, res) => {
+const deletePost = asyncHandler(async (req, res) => {
   res.status(200).json({
     message: `Delete post ${req.params.id}`
   });
-};
+});
 
 export { getPosts, addPost, updatePost, deletePost };
--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import { errorHandler } from './middleware/error.middleware.js';
 import postRoutes from './routes/postRoutes.js';
 
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use('/api/posts', postRoutes);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
