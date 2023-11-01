91f1ff3828f01f2dbec180ada2a5f4cb22601561
avag <avagsargsyan01@gmail.com>
2023-11-01 20:32:13 +0400

    Style UserHeader component

---
+++ client/src/components/UserHeader/UserHeader.jsx
@@ -0,0 +1,61 @@
+import { Link, useNavigate } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../../features/auth/auth.slice';
+import InstagramLogo from '../InstagramLogo/InstagramLogo';
+import styles from './UserHeader.module.scss';
+
+function UserHeader() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate('/login');
+  };
+
+  return (
+    <header className={styles.container}>
+      {/* <div className="logo">
+        <Link to="/">
+          <InstagramLogo />
+        </Link>
+      </div>
+      <ul>
+        <li>
+          <Link to="/">{user && user.name}</Link>
+        </li>
+      </ul>
+      <button onClick={onLogout}>Log out</button> */}
+      <nav>
+        <Link
+          to="/"
+          className={`${styles.link} ${styles.instagramLogoLink}`}>
+          <InstagramLogo
+            className={styles.logo}
+            size="small"
+          />
+        </Link>
+        <ul>
+          <li>
+            <Link
+              to="/"
+              className={styles.userName}>
+              {user && user.name}
+            </Link>
+          </li>
+          <li>
+            <button
+              className={`${styles.logoutBtn} btn-default`}
+              onClick={onLogout}>
+              Log out
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default UserHeader;
