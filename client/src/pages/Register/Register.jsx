b7b8c6422f09ad8a9ac5ce6b51802e26985e3210
avag <avagsargsyan01@gmail.com>
2023-11-03 14:28:31 +0400

    Fix a bug in styling

--- client/src/pages/Register/Register.jsx
+++ client/src/pages/Register/Register.jsx
@@ -50,73 +50,75 @@ function Register() {
   };
 
   return (
-    <section className={styles.container}>
-      <div className={styles.formContainer}>
-        <InstagramLogo className={styles.logo} />
-        <form
-          onSubmit={onSubmit}
-          className={styles.form}>
-          <p className={styles.mainText}>
-            Sign up to see photos and videos from your friends.
-          </p>
-          <a
-            href="#"
-            className={`btn-default ${styles.facebookBtn}`}>
-            <img
-              src="/logos/facebook_logo.png"
-              alt="Facebook"
-            />
-            <span>Log in with Facebook</span>
-          </a>
-          <div className={styles.or}>
-            <div className={styles.line}></div>
-            <div className={styles.orText}>or</div>
-          </div>
-          <FormControl
-            type="email"
-            name="email"
-            value={email}
-            onChange={onChange}>
-            Mobile number or email address
-          </FormControl>
-          <FormControl
-            type="text"
-            name="fullName"
-            value={fullName}
-            onChange={onChange}>
-            Full Name
-          </FormControl>
-          <FormControl
-            type="text"
-            name="name"
-            value={name}
-            onChange={onChange}>
-            Username
-          </FormControl>
-          <FormControl
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChange}>
-            Password
-          </FormControl>
-          <button
-            type="submit"
-            className={`btn-default ${styles.signupBtn}`}>
-            {/* todo: Add a loading spinner */}
-            {isLoading ? 'Loading...' : 'Sign up'}
-          </button>
-        </form>
-      </div>
-      <div className={styles.loginPrompt}>
-        Have an account?&nbsp;
-        <Link
-          to="/login"
-          className={styles.login}>
-          Log in
-        </Link>
-      </div>
-    </section>
+    <div className={styles.pageWrapper}>
+      <section className={styles.container}>
+        <div className={styles.formContainer}>
+          <InstagramLogo className={styles.logo} />
+          <form
+            onSubmit={onSubmit}
+            className={styles.form}>
+            <p className={styles.mainText}>
+              Sign up to see photos and videos from your friends.
+            </p>
+            <a
+              href="#"
+              className={`btn-default ${styles.facebookBtn}`}>
+              <img
+                src="/logos/facebook_logo.png"
+                alt="Facebook"
+              />
+              <span>Log in with Facebook</span>
+            </a>
+            <div className={styles.or}>
+              <div className={styles.line}></div>
+              <div className={styles.orText}>or</div>
+            </div>
+            <FormControl
+              type="email"
+              name="email"
+              value={email}
+              onChange={onChange}>
+              Mobile number or email address
+            </FormControl>
+            <FormControl
+              type="text"
+              name="fullName"
+              value={fullName}
+              onChange={onChange}>
+              Full Name
+            </FormControl>
+            <FormControl
+              type="text"
+              name="name"
+              value={name}
+              onChange={onChange}>
+              Username
+            </FormControl>
+            <FormControl
+              type="password"
+              name="password"
+              value={password}
+              onChange={onChange}>
+              Password
+            </FormControl>
+            <button
+              type="submit"
+              className={`btn-default ${styles.signupBtn}`}>
+              {/* todo: Add a loading spinner */}
+              {isLoading ? 'Loading...' : 'Sign up'}
+            </button>
+          </form>
+        </div>
+        <div className={styles.loginPrompt}>
+          Have an account?&nbsp;
+          <Link
+            to="/login"
+            className={styles.login}>
+            Log in
+          </Link>
+        </div>
+      </section>
+    </div>
   );
 }
 
