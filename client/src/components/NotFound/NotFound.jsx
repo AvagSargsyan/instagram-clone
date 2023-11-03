763daf8dd57e04498f59e7687ffc3c5bce484dfb
avag <avagsargsyan01@gmail.com>
2023-11-03 13:22:10 +0400

    Refactor NotFound page to desplay different content depending on if a user is logged in

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,18 +1,22 @@
 import GuestHeader from '../GuestHeader/GuestHeader';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import UserHeader from '../UserHeader/UserHeader';
 import styles from './NotFound.module.scss';
 
 function NotFound() {
+  const { user } = useSelector((state) => state.auth);
+
   return (
     <div className={styles.container}>
-      <GuestHeader className={styles.header} />
+      {user ? <UserHeader /> : <GuestHeader className={styles.header} />}
       <section className={styles.main}>
         <h2 className={styles.heading}>Sorry, this page isn't available.</h2>
         <p className={styles.info}>
           The link you followed may be broken, or the page may have been
           removed.{' '}
           <Link
-            to="/login"
+            to={user ? '/' : '/login'}
             className={styles.link}>
             Go back to Instagram.
           </Link>
