cb6dd011e30aa2306bb9cacd6d742b60fb9a77ea
avag <avagsargsyan01@gmail.com>
2023-11-10 17:23:02 +0400

    Add time information on post

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,27 +1,38 @@
+import { formatDistanceToNow } from 'date-fns';
 import styles from './Post.module.scss';
 
-const Post = ({ user, post, className }) => (
-  <div className={`${styles.post} ${className}`}>
-    <div className={styles.user}>
-      <img
-        className={styles.userProfilePicture}
-        src={user.profilePictureSrc}
-        alt={`User ${user.fullName}`}
-      />
-      <div className={styles.username}>{post.author.name}</div>
-      <div className={styles.options}>...</div>
+const Post = ({ user, post, className }) => {
+  const timeAgo = formatDistanceToNow(new Date(post.createdAt), {
+    addSuffix: true
+  });
+
+  return (
+    <div className={`${styles.post} ${className}`}>
+      <section className={styles.user}>
+        <img
+          className={styles.userProfilePicture}
+          src={user.profilePictureSrc}
+          alt={`User ${user.fullName}`}
+        />
+        <div className={styles.username}>{post.author.name}</div>
+        <div className={styles.options}>...</div>
+      </section>
+      <section className={styles.postImageContainer}>
+        <img
+          className={styles.image}
+          src={post.imageSrc}
+          alt={`${user.name}'s post`}
+        />
+      </section>
+      <section className={styles.postInfo}>
+        <p className={styles.postContent}>
+          <span className={styles.inlineUsername}>{user.name}</span>{' '}
+          {post.content}
+        </p>
+        <p className={styles.time}>{timeAgo}</p>
+      </section>
     </div>
-    <div className={styles.postImageContainer}>
-      <img
-        className={styles.image}
-        src={post.imageSrc}
-        alt={`${user.name}'s post`}
-      />
-    </div>
-    <p className={styles.postContent}>
-      <span className={styles.inlineUsername}>{user.name}</span> {post.content}
-    </p>
-  </div>
-);
+  );
+};
 
 export default Post;
